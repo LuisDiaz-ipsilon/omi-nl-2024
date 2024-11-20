b1bd72b828955ad14b435eeedcd90b11292e7bbc
LuisDiaz-ipsilon <54384617+LuisDiaz-ipsilon@users.noreply.github.com>
2024-11-19 20:22:03 -0600

    env add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,8 @@ export const environment = {
   },
   supabaseUrl: 'https://ekanlpkfqhhnbnyzbxtc.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrYW5scGtmcWhobmJueXpieHRjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMTc2NDEyOSwiZXhwIjoyMDM3MzQwMTI5fQ.EoZFaAg0w9ONABZPJsrvUOdguzF_tcBGo8HqSnAVKqI',
-  authSiteURL: 'http://localhost:3000/api/auth',
-  archivosSiteURL: 'http://localhost:3000/api/archivos'
+  //authSiteURL: 'http://localhost:3000/api/auth',
+  authSiteURL: 'https://nest-backend-omi.vercel.app/api/auth',
+  archivosSiteURL: 'https://nest-backend-omi.vercel.app/api/archivos'
 };
 //GITIGNORE aver
